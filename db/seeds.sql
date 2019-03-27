5dcfaae7b7cb0c8da0640d5d2b0552c2ddc99ceb
Jose Antunez <jangel5673@yahoo.com>
2019-03-27 15:16:19 -0700

    Burgers app

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, devoured) VALUES ("Double Double", true);
+INSERT INTO burgers (name, devoured) VALUES ("Triple", true);
+INSERT INTO burgers (name, devoured) VALUES ('4By4', true);
