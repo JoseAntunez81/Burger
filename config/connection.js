eb7e0949b222a221c1c094ba20e169ef61f48ceb
Jose Antunez <jangel5673@yahoo.com>
2019-04-12 11:51:42 -0700

    connection changes

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "Ja15665207$",
+  password: "UclaBootcamp",
   database:"burger_db"
 });
 
